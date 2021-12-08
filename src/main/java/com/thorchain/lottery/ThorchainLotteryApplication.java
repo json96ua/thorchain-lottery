8e398e0d589d74a9f4995bddd96a58d97fc244ce
json96ua <Heavymetal1>
2021-12-08 11:56:55 +0200

    Initial commit

---
+++ src/main/java/com/thorchain/lottery/ThorchainLotteryApplication.java
@@ -0,0 +1,13 @@
+package com.thorchain.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThorchainLotteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThorchainLotteryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thorchain/lottery/ThorchainLotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thorchain.lottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThorchainLotteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
