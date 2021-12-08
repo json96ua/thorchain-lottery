6fdb0d7912fc237d7de76f711c8011a48c76fd75
json96ua <Heavymetal1>
2021-12-08 18:28:51 +0200

    Add class for constants

---
+++ src/main/java/com/thorchain/lottery/common/Constants.java
@@ -0,0 +1,8 @@
+package com.thorchain.lottery.common;
+
+import java.math.BigInteger;
+
+public final class Constants {
+    public final static int DISCHARGE = 100000000;
+    public final static int AVAILABLE_LPU_AMOUNT = 10;
+}
